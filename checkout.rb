2e36fb2bb94484aaed5ebd6c5f8b55cacc89bd31
exwarvlad <exwarvlad@gmail.com>
2019-11-08 15:47:51 +0200

    refactor check_discount_static logic

--- checkout.rb
+++ checkout.rb
@@ -2,8 +2,8 @@ require_relative 'item'
 
 class Checkout
   def initialize(promotional_rules)
-    # {discount_percent: {percent: 10, over_spend: 60}, discount_static: {amount: 2, discount: 8.5}}
-    @discount_percent = promotional_rules[:discount_percent]
+    # {discount_in_percent: {percent: 10, over_spend: 60}, discount_static: {amount: 2, discount: 8.5}}
+    @discount_percent = promotional_rules[:discount_in_percent]
     @discount_static = promotional_rules[:discount_static]
     @card = []
   end
@@ -15,16 +15,32 @@ class Checkout
   def total
     total_price = @card.map(&:price).sum
     total_price -= check_discount_static
-    total_price -= total_price / 100 * @discount_percent[:percent].to_f if total_price >= @discount_percent[:over_spend]
+    total_price -= total_price / 100 * @discount_percent[:percent] if discount_in_percent?(total_price)
     total_price
   end
 
   private
 
   def check_discount_static
-    amount = 0
-    items = @card.select { |item| @card.count(item) >= @discount_static[:amount] }
-    items.uniq.each { |item| amount += (item.price * @card.count(item) - @discount_static[:discount]) }
-    amount
+    # check discount static in total sum
+    # 3 items, discount_static_amount -- 1 item + discount_static_discount
+    # 2 items, discount_static_amount -- discount_static_discount
+    discount = 0
+    @card.uniq.each do |item|
+      items_price = @card.count(item) * item.price
+
+      if @card.count(item) > @discount_static[:amount]
+        discount += items_price - @card.count(item) / @discount_static[:amount] * item.price
+        discount -= @discount_static[:discount] * (@card.count(item) - @discount_static[:amount])
+      elsif @card.count(item) == @discount_static[:amount]
+        discount += items_price
+        discount -= @card.count(item) / @discount_static[:amount] * @discount_static[:discount]
+      end
+    end
+    discount
+  end
+
+  def discount_in_percent?(total_price)
+    total_price >= @discount_percent[:over_spend]
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,19 +1,41 @@
 require 'rspec'
 require_relative '../checkout'
 
-describe 'checkout' do
-  let!(:rules) { {discount_percent: {percent: 10, over_spend: 60}, discount_static: {amount: 2, discount: 8.5}} }
+describe 'Checkout .total' do
+  let!(:rules) { {discount_in_percent: {percent: 10, over_spend: 42}, discount_static: {amount: 2, discount: 8.5}} }
   let!(:item1) { Item.new(code: 001, name: 'bum-shaka-laka', price: 42) }
-  let!(:item2) { Item.new(code: 002, name: 'banana', price: 61) }
+  let!(:item2) { Item.new(code: 002, name: 'banana', price: 10) }
 
-  it '.total' do
-    checkout = Checkout.new(rules)
-    checkout.add(item1)
-    checkout.add(item1)
-    checkout.add(item2)
-    price = rules[:discount_static][:discount] + item2.price
-    discount = (rules[:discount_static][:discount] + item2.price) / 100 * rules[:discount_percent][:percent]
+  it 'discount_in_percent' do
+    co = Checkout.new(rules)
+    co.add(item1)
 
-    expect(checkout.total).to eq(price - discount)
+    expect(co.total).to eq(item1.price.to_f - item1.price / 100 * rules[:discount_in_percent][:percent])
+  end
+
+  it 'discount_static' do
+    co = Checkout.new(rules)
+    co.add(item2)
+    co.add(item2)
+    co.add(item2)
+
+    co2 = Checkout.new(rules)
+    co2.add(item2)
+    co2.add(item2)
+
+    # expect(co.total).to eq(rules[:discount_static][:discount] + item2.price)
+    expect(co2.total).to eq(rules[:discount_static][:discount])
+  end
+
+  it 'discount_in_percent and discount_static' do
+    co = Checkout.new(rules)
+    co.add(item1)
+    co.add(item2)
+    co.add(item2)
+    co.add(item2)
+    with_statick_discount = rules[:discount_static][:discount] + item2.price + item1.price
+    total_sum = with_statick_discount - with_statick_discount / 100 * rules[:discount_in_percent][:percent]
+
+    expect(co.total).to eq total_sum
   end
 end
