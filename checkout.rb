9f9f7e215f7e729956136a6bbfffa989331e5bfe
exwarvlad <exwarvlad@gmail.com>
2019-11-09 12:32:44 +0200

    optimizated check_discount_static

--- checkout.rb
+++ checkout.rb
@@ -1,4 +1,4 @@
-require_relative 'item'
+require_relative 'lib/item'
 
 class Checkout
   def initialize(promotional_rules)
@@ -6,38 +6,28 @@ class Checkout
     @discount_percent = promotional_rules[:discount_in_percent]
     @discount_static = promotional_rules[:discount_static]
     @card = []
+    @discount = 0
   end
 
   def add(item)
     @card.push(item)
+    check_discount_static(@card.count(item), item)
   end
 
   def total
-    total_price = @card.map(&:price).sum
-    total_price -= check_discount_static
+    total_price = @card.map(&:price).sum - @discount
     total_price -= total_price / 100 * @discount_percent[:percent] if discount_in_percent?(total_price)
     total_price
   end
 
   private
 
-  def check_discount_static
-    # check discount static in total sum
-    # 3 items, discount_static_amount -- 1 item + discount_static_discount
-    # 2 items, discount_static_amount -- discount_static_discount
-    discount = 0
-    @card.uniq.each do |item|
-      items_price = @card.count(item) * item.price
-
-      if @card.count(item) > @discount_static[:amount]
-        discount += items_price - @card.count(item) / @discount_static[:amount] * item.price
-        discount -= @discount_static[:discount] * (@card.count(item) - @discount_static[:amount])
-      elsif @card.count(item) == @discount_static[:amount]
-        discount += items_price
-        discount -= @card.count(item) / @discount_static[:amount] * @discount_static[:discount]
-      end
+  def check_discount_static(items_counter, item)
+    if items_counter >= @discount_static[:amount]
+      new_price = (items_counter / @discount_static[:amount]) * @discount_static[:discount]
+      old_price = (items_counter - (items_counter - @discount_static[:amount])) * item.price
+      @discount = old_price - new_price
     end
-    discount
   end
 
   def discount_in_percent?(total_price)
